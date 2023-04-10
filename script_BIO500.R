2ff2900af83085c82042436bcd93917b3a06a8cd
YArchambault12 <yuriko.archambault@usherbrooke.ca>
2023-04-10 14:58:38 -0400

    add file via upload

--- script_BIO500.R
+++ script_BIO500.R
@@ -310,6 +310,20 @@ dbDisconnect(con)
 
 <<<<<<< HEAD
 #igraph
+interaction_df <- data.frame(etudiantA = collaboration$etudiant1, etudiantB = collaboration$etudiant2, stringsAsFactors = TRUE)
+interaction_matrice <- as.matrix(interaction_df)
+interaction_ig <- graph.edgelist(interaction_matrice , directed=TRUE)
+kamada_layout <- layout.kamada.kawai(interaction_ig)
+plot(interaction_ig, 
+     layout = kamada_layout, 
+     vertex.size = 14,
+     vertex.color = "red",
+     vertex.frame.color = NA,
+     vertex.label.cex = 1.2,
+     edge.curved = .2,
+     edge.arrow.size = .3,
+     edge.width = 1)
+
 interaction <- matrix(nrow = 395, ncol = 395) 
 colnames(interaction) <- as.character(etudiant[,1])
 rownames(interaction) <- as.character(etudiant[,1])
