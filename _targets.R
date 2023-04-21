d93a4bab34f2450ef6ccb4c03b5f9182b8ae9366
Marie-EveGagne <marie.g9985@gmail.com>
2023-04-21 19:16:35 -0400

    merge conflict

--- _targets.R
+++ _targets.R
@@ -6,33 +6,58 @@ setwd("C:/Users/Marie-Eve/OneDrive - USherbrooke/Bureau/UdeS/methode_comp/travai
 source("R/prep_donnees.R")
 source("R/correction.R")
 tar_option_set(packages = c("RSQLite", 'stringr','dplyr','data.table','stringdist','igraph','purrr'))
+tar_option_set(packages = c("RSQLite", 'stringr','dplyr','data.table','stringdist','igraph'))
+
 list(
-  tar_target(
+   tar_target(
     name = path,
     command = "/donnees_BIO500",
     format = "file"
-  ),
-  tar_target(
-    name = file_paths,
-    command = list.files(path, full.names = TRUE)
-  ),
-  tar_target(
-    name = data,
-    command = prep_donnees(file_paths)
-  ),
-  tar_target(
-    name = donnees_BIO500,
-    command = correction(data)
-  ),
-  tar_target(
-    name = SQL,
-    command = inj_req(donnees_BIO500)
-  ),
-  tar_target(
-    name = graphique,
-    command = tbl_fig(SQL)
-  ),
-)
+   ),
+   tar_target(
+     name = file_paths,
+     command = list.files(path, full.names = TRUE)
+   ),
+   tar_target(
+     name = data_collab,
+     command = prep_collab(file_paths),
+   ),
+   tar_target(
+     name = data_cours,
+     command = prep_cours(file_paths)
+   ),
+   tar_target(
+     name = data_etudiant,
+     command = prep_etudiants(file_paths)
+   ),
+   tar_target(
+     name = etudiant_clean,
+     command = corr_etd(data_etudiant)
+   ),
+   tar_target(
+     name = collab_clean,
+     command = corr_collab(data_collab)
+   ))
+#   tar_target(
+#     name = SQL,
+#     command = inj_req(donnees_BIO500)
+#   ),
+#   tar_target(
+#     name = graphique,
+#     command = tbl_fig(SQL)
+#   ),
+# )
+
+
+
+
+
+
+
+
+
+
+
 
 
 
