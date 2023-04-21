63793a0f65ac149ddc789ed2254544f0edb1a5df
Marie-EveGagne <marie.g9985@gmail.com>
2023-04-21 10:20:15 -0400

    merge conflict

--- script_BIO500.R
+++ script_BIO500.R
@@ -276,7 +276,7 @@ dbSendQuery(con, tbl_collaboration)
 
 dbWriteTable(con, append = TRUE, name = "tbl_cours", value = cours, row.names = FALSE)
 dbWriteTable(con, append = TRUE, name = "tbl_etudiant", value = etudiant, row.names = FALSE)
-dbWriteTable(con, append = TRUE, name = "tbl_collaboration", value = collaboration, row.names = FALSE)
+dbWriteTable(con, append = TRUE, name = "tbl_collaboration", value = Collab_corr, row.names = FALSE)
 
 #Répondre aux questions pour le cours de BIO500 et enregistrer les reponses dans un csv
 
